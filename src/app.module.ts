9f26cfeac934fda454695c8426e10aeabb744f82
andrey <guschaninov@yandex.ru>
2021-03-31 09:02:47 +0300

    nm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ContactsModule } from './contacts/contacts.module';
 import { PipelinesModule } from './pipelines/pipelines.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       // url: 'postgres://ovnksvcl:OZW-Nq09m4pcdyJEjGaCo_MziBCGR9eO@balarama.db.elephantsql.com:5432/ovnksvcl',
